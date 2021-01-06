ee9e613cc9eec90c5c1d9e08dd2be8d0d6968264
Irreversible <59417306+Irreversibles@users.noreply.github.com>
2021-01-06 17:55:16 +0800

    Update qqreadck.js

--- Task/qqreadck.js
+++ Task/qqreadck.js
@@ -1,11 +1,6 @@
-/* ziye  导出企鹅ck专用    务必下载或者复制到本地使用
+/* Irreversibles  导出企鹅ck专用    务必下载或者复制到本地使用
 
-github地址     https://github.com/ziye12/JavaScript
-TG频道地址     https://t.me/ziyescript
-TG交流群       https://t.me/joinchat/AAAAAE7XHm-q1-7Np-tF3g
-boxjs链接      https://raw.githubusercontent.com/ziye12/JavaScript/master/Task/ziye.boxjs.json
 
-转载请备注个名字，谢谢
 
 
 
